21f9760143abb32a1f3b64e8d70cc2f77a2952c5
Hugo Baraúna <hugo.barauna@plataformatec.com.br>
2013-12-26 12:24:13 -0200

    Finish the fizz buzz example

--- chapter-5/fizz_buzz.exs
+++ chapter-5/fizz_buzz.exs
@@ -1,11 +1,25 @@
-fizz_buzz = fn
+# A solution to the FizzBuzz problem withtout conditionals!!!
+
+fizz_word = fn
   0, 0, _ -> "FizzBuzz"
   0, _, _ -> "Fizz"
   _, 0, _ -> "Buzz"
   _, _, n -> n
 end
 
-"FizzBuzz" = fizz_buzz.(0, 0, 1)
-"Fizz" = fizz_buzz.(0, 1, 1)
-"Buzz" = fizz_buzz.(1, 0, 1)
-2 = fizz_buzz.(1, 1, 2)
+"FizzBuzz" = fizz_word.(0, 0, 1)
+"Fizz" = fizz_word.(0, 1, 1)
+"Buzz" = fizz_word.(1, 0, 1)
+2 = fizz_word.(1, 1, 2)
+
+fizz_buzz = fn
+  n -> fizz_word.(rem(n, 3), rem(n, 5), n)
+end
+
+"Buzz" = fizz_buzz.(10)
+11 = fizz_buzz.(11)
+"Fizz" = fizz_buzz.(12)
+13 = fizz_buzz.(13)
+14 = fizz_buzz.(14)
+"FizzBuzz" = fizz_buzz.(15)
+16 = fizz_buzz.(16)
